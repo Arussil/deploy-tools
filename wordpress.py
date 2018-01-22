7826bbdc7b677e930e69189838fb2498bbb580db
Fabio Piras <fabio@molluscobalena.it>
2018-01-22 14:05:04 +0100

    First commit

---
+++ wordpress.py
@@ -0,0 +1,28 @@
+import configparser, os
+
+from fabric.api import local, lcd
+
+ROOT = os.path.dirname(os.path.abspath(__file__))
+
+def createVagrant(config):
+    #Download VVV on the path
+    path = config['LOCAL']['PATH']
+    with lcd(path):
+        local('git clone -b master git://github.com/Varying-Vagrant-Vagrants/VVV.git ./vagrant')
+  
+    #Copy the VVV config file to the right path
+    vvv_config_file = os.path.join(
+        os.path.sep, ROOT,
+        'projects',
+        config['PROJECT']['NAME'],
+        'vvv-custom.yml'
+    )
+    with lcd(os.path.join(os.path.sep, path, 'vagrant')):
+        local('cp {} ./'.format(vvv_config_file))
+        #Instal vagrant plugins
+        local('vagrant plugin install vagrant-hostsupdater vagrant-triggers vagrant-vbguest')
+        #Install the machine
+        local('vagrant up')
+
+def createDevelopmentEnv(config):
+    createVagrant(config)
